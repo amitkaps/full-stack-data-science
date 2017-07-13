b46674167bdc9f3f95b9bc8b897f1b40550a7131
Anand Chitipothu <anandology@gmail.com>
2017-07-13 12:03:26 +0200

    Fixed the loose ends in the webapp.
    * moved the code to load/save loans to loansdb.py
    * Added stubs for finding grade and probability of default
    * Showing a thankyou message after loan request is submitted.

---
+++ credit-risk/webapp/loansdb.py
@@ -0,0 +1,60 @@
+"""The loans database.
+
+This module manages saving the loans into files and reading them back.
+"""
+import datetime
+import pathlib
+import json
+from firefly.client import Client
+
+def get_loans():
+    """Returns all the existing loans.
+    """
+    path = pathlib.Path("loans")
+    path.mkdir(exist_ok=True)
+
+    loans = []
+    for p in path.glob("*.loan"):
+        loan = json.loads(p.read_text(encoding='utf-8'))
+        loans.append(loan)
+    return sorted(loans, reverse=True, key=lambda loan: loan['timestamp'])
+
+def save_loan(name, email, amount, duration, age, ownership, income):
+    grade = find_credit_grade(email)
+    proba = predict_proba(
+                amount=amount, years=duration, age=age,
+                ownership=ownership, income=income, grade=grade)
+    timestamp = datetime.datetime.now().isoformat()
+
+    loan = dict(
+        name=name,
+        email=email,
+        amount=amount,
+        duration=duration,
+        age=age,
+        ownership=ownership,
+        income=income,
+        grade=grade,
+        p_default=proba,
+        timestamp=timestamp)
+
+    _write_loan(loan)
+
+def _write_loan(loan):
+    root = pathlib.Path("loans")
+    root.mkdir(exist_ok=True)
+    path = root.joinpath(loan['timestamp'] + ".loan")
+    path.write_text(json.dumps(loan))
+    print("Saved loan", path)
+
+def find_credit_grade(email):
+    """Find the credit grade of a person using the Credit Grade API.
+    """
+    # FIXME: Use the credit grade API
+    return "A"
+
+def predict_proba(amount, years, age, ownership, income, grade):
+    """Predicts the probablity of default for given inputs using the model service.
+    """
+    # FIXME: Use the Credit Risk Prediction service to find the probability
+    return 0.5
--- credit-risk/webapp/webapp.py
+++ credit-risk/webapp/webapp.py
@@ -1,37 +1,15 @@
 from flask import Flask, render_template, redirect
 from forms import LoanForm
-import datetime
-import pathlib
-import json
+import loansdb
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abrakadabra'
-
-def get_loans():
-    path = pathlib.Path("loans")
-    path.mkdir(exist_ok=True)
-
-    loans = []
-    for p in path.glob("*.loan"):
-        loan = json.loads(p.read_text(encoding='utf-8'))
-        loans.append(loan)
-    return loans
-
-def save_loan(**fields):
-    root = pathlib.Path("loans")
-    root.mkdir(exist_ok=True)
-
-    timestamp = datetime.datetime.now().isoformat()
-    fields['timestamp'] = timestamp
-    path = root.joinpath(timestamp + ".loan")
-    path.write_text(json.dumps(fields))
-    print("Saved loan", path)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     form = LoanForm()
     if form.validate_on_submit():
-        save_loan(
+        loansdb.save_loan(
             amount=form.amount.data,
             duration=form.duration.data,
             name=form.name.data,
@@ -39,12 +17,12 @@ def index():
             age=form.age.data,
             income=form.income.data,
             ownership=form.ownership.data)
-        return redirect("/")
+        return render_template("thankyou.html", name=form.name.data)
     return render_template("index.html", form=form)
 
 @app.route("/admin")
 def admin():
-    loans = get_loans()
+    loans = loansdb.get_loans()
     return render_template("admin.html", loans=loans)
 
 if __name__ == "__main__":
