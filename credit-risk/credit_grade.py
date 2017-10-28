269422f4262943380e9cacf04e0a3d0b43f922ed
Anand Chitipothu <anandology@gmail.com>
2017-10-28 10:27:25 +0530

    Added some docs to credit_grade.py

--- credit-risk/credit_grade.py
+++ credit-risk/credit_grade.py
@@ -1,11 +1,18 @@
 """Program to find the credit grade of a person.
+
+usage:
+    
+    firefly credit_grade.find_credit_grade
 """
 import zlib
 import random
 
 def find_credit_grade(email):
     """Returns the credit grade of the person identified by the given email address.
-    
+
+    The credit grade is generated randomly using the email as the seed to the random
+    number generator.    
+
     The credit grade can be either A, B, C, D, E, F or G.
     """
     # since we need to give the same grade everytime the function is called
